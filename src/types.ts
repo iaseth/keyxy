084e500924f270856a72ffe53331bb8f2ba8ba77
Ankur Seth <iankurseth@gmail.com>
2023-12-12 13:27:29 +0530

    Added isAlpha, isDigit and isSymbol attributes to KeyboardKey

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,10 @@
 export type KeyboardKey = {
 	keynames: string[],
 	row: 0|1|2|3|4|5,
-	column: 0|1|2|3|4|5|6|7|8|9|10|11|12|13
+	column: 0|1|2|3|4|5|6|7|8|9|10|11|12|13,
+	isAlpha: boolean,
+	isDigit: boolean,
+	isSymbol: boolean
 };
 
 export type KeyboardLayout = {
